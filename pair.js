dc4730653bc581445a8afdea4e1995f763c0b845
dominiquenomel028-wq <dominiquenomel028@gmail.com>
2026-02-08 06:33:07 +0000

    Update pair.js

--- pair.js
+++ pair.js
@@ -81,7 +81,7 @@ const config = {
 
 // ---------------- MONGO SETUP ----------------
 
-const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://malvintech11_db_user:0SBgxRy7WsQZ1KTq@cluster0.xqgaovj.mongodb.net/?appName=Cluster0'; //we need to create a mongodb url soon
+const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://dominiquenomel028_db_user:wwu5fFaNpJHcpHLW@cluster0.eala7zq.mongodb.net/?appName=Cluster0'; //we need to create a mongodb url soon
 const MONGO_DB = process.env.MONGO_DB || 'Free_Mini';
 
 let mongoClient, mongoDB;
@@ -2181,5 +2181,6 @@ initMongo().catch(err => console.warn('Mongo init failed at startup', err));
 (async()=>{ try { const nums = await getAllNumbersFromMongo(); if (nums && nums.length) { for (const n of nums) { if (!activeSockets.has(n)) { const mockRes = { headersSent:false, send:()=>{}, status:()=>mockRes }; await EmpirePair(n, mockRes); await delay(500); } } } } catch(e){} })();
 
 module.exports = router;
+
 
 
